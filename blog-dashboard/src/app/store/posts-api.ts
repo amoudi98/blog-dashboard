c5f4796329b614e0a2b566d19c1280e543349be4
amoudi98 <amoudi98@gmail.com>
2025-04-13 02:26:27 +0400

    Added posts api and store

---
+++ blog-dashboard/src/app/store/posts-api.ts
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Post {
+    id: number;
+    title: string;
+    body: string;
+    author: string;
+}
+
+export const postsApi = createApi({
+    reducerPath: 'postsApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+    endpoints: (builder) => ({
+        getPosts: builder.query<{ data: Post[]; totalPages: number }, number | void>({
+            query: (page = 1) => `/posts?_page=${page}&_limit=6`, // adjust page size if needed
+            transformResponse: (response: Post[], meta) => {
+                const totalCount = Number(meta?.response?.headers.get('X-Total-Count') || 0);
+                const totalPages = Math.ceil(totalCount / 6); // same limit as in query
+                return { data: response, totalPages };
+            },
+        }),
+        getPostById: builder.query<Post, number>({
+            query: (id) => `posts/${id}`,
+        }),
+        addPost: builder.mutation<Post, Partial<Post>>({
+            query: (post) => ({
+                url: 'posts',
+                method: 'POST',
+                body: post,
+            }),
+        }),
+    }),
+});
+
+export const { useGetPostsQuery, useGetPostByIdQuery, useAddPostMutation, util: postsApiUtil} = postsApi;
